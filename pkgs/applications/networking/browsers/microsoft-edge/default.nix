9b49b1a14b17175da431b517655dbac21c932239
reimeri <reimeri@tuta.io>
2022-05-03 19:11:58 +0300

    microsoft-edge: 98.0.1108.56 -> 101.0.1210.32

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -1,20 +1,20 @@
 {
   beta = import ./browser.nix {
     channel = "beta";
-    version = "99.0.1150.16";
+    version = "101.0.1210.31";
     revision = "1";
-    sha256 = "sha256:0qsgs889d6qwxz9qf42psmjqfhmrqgp07srq5r38npl5pncr137h";
+    sha256 = "sha256:0f477hk0d4v3fwp4yzcsbbvc0slw2pc986ssjd2283q5skgv3a49";
   };
   dev = import ./browser.nix {
     channel = "dev";
-    version = "100.0.1163.1";
+    version = "102.0.1235.1";
     revision = "1";
-    sha256 = "sha256:153faqxyw5f5b6cqnvd71dl7941znkzci8dwbcgaxway0b6882jq";
+    sha256 = "sha256:1byr8r38sl219cpxc32y23n6bfqvs3rvq1kclmsa22ch54sqcwdl";
   };
   stable = import ./browser.nix {
     channel = "stable";
-    version = "98.0.1108.56";
+    version = "101.0.1210.32";
     revision = "1";
-    sha256 = "sha256:03jbj2s2fs60fzfgsmyb284q7nckji87qgb86mvl5g0hbl19aza7";
+    sha256 = "sha256:1ybpj6rlhx9szvblqy1s2dbnakm9iv7s3xz6y3c53g46al43hi2f";
   };
 }
